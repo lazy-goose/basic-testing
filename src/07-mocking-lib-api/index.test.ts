c27e1fb69fc5af26922f6410c5aec28884928ad6
Lazy Goose <shamal.ma.rss@gmail.com>
2024-02-10 01:56:46 +0300

    feat: impl 07-mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,60 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios);
+
+// I guess it should be exported from './index'
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    // Mocked modules are scoped within the file that calls jest.mock
+    // No need for jest.unmock()
+    jest.unmock('axios');
+    jest.useRealTimers();
+  });
+
+  beforeEach(() => {
+    // AxiosInstance extends interface Axios but is not an actual instance
+    mockedAxios.create.mockReturnThis();
+    mockedAxios.get.mockResolvedValue(successResponseData);
+  });
+
+  afterEach(() => {
+    jest.runAllTimers();
+    // No need to restore due to "restoreMocks: true" flag in jest.config.js
+    jest.restoreAllMocks();
+  });
+
+  const successResponseData = {
+    data: 'Fake Data',
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config: {},
+    request: {},
+  };
+  const urlPath = 'nonExistingPath';
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi(urlPath);
+    expect(axios.create).toBeCalledWith({ baseURL: BASE_URL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi(urlPath);
+    expect(axios.get).toBeCalledWith(urlPath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    await expect(throttledGetDataFromApi(urlPath)).resolves.toBe(
+      successResponseData.data,
+    );
   });
 });
